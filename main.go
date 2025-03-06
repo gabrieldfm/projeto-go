63d0f8eefd5f26d11652a235080978bbc5244411
gabrieldfm <gabrieldfm13@gmail.com>
2025-03-06 18:26:47 -0300

    add delete and update

--- main.go
+++ main.go
@@ -36,6 +36,36 @@ func (c *ContactService) Create(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func (c *ContactService) Delete(w http.ResponseWriter, r *http.Request, id int) {
+	w.Header().Set("Content-Type", "application/json")
+	if _, ok := c.Contacts[id]; ok {
+		delete(c.Contacts, id)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func (c *ContactService) Update(w http.ResponseWriter, r *http.Request, id int) {
+	w.Header().Set("Content-Type", "application/json")
+	var contact Contact
+
+	err := json.NewDecoder(r.Body).Decode(&contact)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if _, ok := c.Contacts[id]; ok {
+		contact.Id = id
+		c.Contacts[id] = contact
+
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
 func (c *ContactService) List(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var contacts []Contact
@@ -56,7 +86,27 @@ func (c *ContactService) Get(w http.ResponseWriter, r *http.Request, id int) {
 	}
 }
 
-func handlegetContacts(w http.ResponseWriter, r *http.Request, service *ContactService) {
+func handleUpdateContacts(w http.ResponseWriter, r *http.Request, service *ContactService) {
+	q := r.URL.Query()
+	if q.Get("id") != "" {
+		id, _ := strconv.Atoi(q.Get("id"))
+		service.Update(w, r, id)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func handleDeleteContacts(w http.ResponseWriter, r *http.Request, service *ContactService) {
+	q := r.URL.Query()
+	if q.Get("id") != "" {
+		id, _ := strconv.Atoi(q.Get("id"))
+		service.Delete(w, r, id)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func handleGetContacts(w http.ResponseWriter, r *http.Request, service *ContactService) {
 	q := r.URL.Query()
 	if q.Get("id") != "" {
 		id, _ := strconv.Atoi(q.Get("id"))
@@ -73,9 +123,13 @@ func main() {
 	mux.HandleFunc("/contacts", func(w http.ResponseWriter, req *http.Request) {
 		switch req.Method {
 		case http.MethodGet:
-			handlegetContacts(w, req, service)
+			handleGetContacts(w, req, service)
 		case http.MethodPost:
 			service.Create(w, req)
+		case http.MethodDelete:
+			handleDeleteContacts(w, req, service)
+		case http.MethodPut:
+			handleUpdateContacts(w, req, service)
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
